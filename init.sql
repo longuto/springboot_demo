d1519517d47468db22a7d9e771e313b131c949df
longuto <1592404240@qq.com>
2019-03-22 16:54:22 +0800

    增加业务数据库

--- init.sql
+++ init.sql
@@ -11,11 +11,91 @@
  Target Server Version : 50553
  File Encoding         : 65001
 
- Date: 19/03/2019 15:33:05
+ Date: 22/03/2019 16:53:13
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for oper_log
+-- ----------------------------
+DROP TABLE IF EXISTS `oper_log`;
+CREATE TABLE `oper_log`  (
+  `id` bigint(20) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `username` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createtime` datetime NULL DEFAULT NULL,
+  `classname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `methodname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `params` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `durationtime` decimal(10, 0) NULL DEFAULT NULL,
+  `ip` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `location` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for pda_in_shelf
+-- ----------------------------
+DROP TABLE IF EXISTS `pda_in_shelf`;
+CREATE TABLE `pda_in_shelf`  (
+  `pickid` int(11) UNSIGNED NOT NULL COMMENT '货位主键',
+  `skuid` int(11) UNSIGNED NOT NULL COMMENT 'sku主键',
+  `skubatch` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '批次号',
+  `qty` int(18) NOT NULL,
+  `editor` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `edittime` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`pickid`, `skuid`, `skubatch`, `qty`) USING BTREE,
+  INDEX `skuid`(`skuid`) USING BTREE,
+  CONSTRAINT `pda_in_shelf_ibfk_1` FOREIGN KEY (`pickid`) REFERENCES `pub_pick` (`pid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `pda_in_shelf_ibfk_2` FOREIGN KEY (`skuid`) REFERENCES `pub_sku` (`sid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for pub_pick
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_pick`;
+CREATE TABLE `pub_pick`  (
+  `pid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `number` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '货位名称',
+  `status` tinyint(4) NULL DEFAULT 1 COMMENT '状态：0禁用，1启用',
+  `skuid` int(11) NULL DEFAULT NULL COMMENT 'sku外键',
+  `stockid` int(11) NULL DEFAULT NULL COMMENT '仓库外键',
+  `totalqty` int(11) NULL DEFAULT NULL COMMENT '总容量',
+  `usedqty` int(11) NULL DEFAULT NULL COMMENT '已用容量',
+  PRIMARY KEY (`pid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for pub_sku
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_sku`;
+CREATE TABLE `pub_sku`  (
+  `sid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `number` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` tinyint(4) NULL DEFAULT 1 COMMENT '状态：禁用是0，启用是1',
+  `type` tinyint(4) NULL DEFAULT 0 COMMENT '类型：0是无批次号，1是启用批次号',
+  `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品说明',
+  PRIMARY KEY (`sid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for pub_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_stock`;
+CREATE TABLE `pub_stock`  (
+  `sid` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '仓库名称',
+  `number` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '仓库编码（仓库别名）',
+  `type` tinyint(4) NULL DEFAULT 0 COMMENT '仓库类型：0单仓，1启用货位',
+  `status` tinyint(4) NULL DEFAULT 0 COMMENT '状态：0是禁用，1是启用',
+  `createtime` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `edittime` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `editor` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`sid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for sys_permission
@@ -34,6 +114,13 @@ CREATE TABLE `sys_permission`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES (1, b'0', '用户管理', 0, '0/', 'userInfo:view', 'menu', 'userInfo/userList');
+INSERT INTO `sys_permission` VALUES (2, b'0', '用户添加', 1, '0/1', 'userInfo:add', 'button', 'userInfo/userAdd');
+INSERT INTO `sys_permission` VALUES (3, b'0', '用户删除', 1, '0/1', 'userInfo:del', 'button', 'userInfo/userDel');
+
+-- ----------------------------
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
@@ -44,6 +131,13 @@ CREATE TABLE `sys_role`  (
   `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (1, b'0', '管理员', 'admin');
+INSERT INTO `sys_role` VALUES (2, b'0', 'VIP会员', 'vip');
+INSERT INTO `sys_role` VALUES (3, b'1', 'test', 'test');
 
 -- ----------------------------
 -- Table structure for sys_role_permission
@@ -57,6 +151,13 @@ CREATE TABLE `sys_role_permission`  (
 ) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
+-- Records of sys_role_permission
+-- ----------------------------
+INSERT INTO `sys_role_permission` VALUES (1, 1);
+INSERT INTO `sys_role_permission` VALUES (2, 1);
+INSERT INTO `sys_role_permission` VALUES (3, 2);
+
+-- ----------------------------
 -- Table structure for sys_user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_role`;
@@ -66,6 +167,11 @@ CREATE TABLE `sys_user_role`  (
   INDEX `FKgkmyslkrfeyn9ukmolvek8b8f`(`uid`) USING BTREE,
   INDEX `FKhh52n8vd4ny9ff4x9fb8v65qx`(`role_id`) USING BTREE
 ) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Fixed;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES (1, 1);
 
 -- ----------------------------
 -- Table structure for user_info
@@ -81,17 +187,9 @@ CREATE TABLE `user_info`  (
   PRIMARY KEY (`uid`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-
-INSERT INTO `user_info` (`uid`,`username`,`name`,`password`,`salt`,`state`) VALUES ('1', 'admin', '管理员', 'd3c59d25033dbf980d29554025c23a75', '8d78869f470951332959580424d4bf4f', 0);
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (1,0,'用户管理',0,'0/','userInfo:view','menu','userInfo/userList');
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (2,0,'用户添加',1,'0/1','userInfo:add','button','userInfo/userAdd');
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (3,0,'用户删除',1,'0/1','userInfo:del','button','userInfo/userDel');
-INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (1,0,'管理员','admin');
-INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (2,0,'VIP会员','vip');
-INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (3,1,'test','test');
-INSERT INTO `sys_role_permission` (`permission_id`,`role_id`) VALUES (1,1);
-INSERT INTO `sys_role_permission` (`permission_id`,`role_id`) VALUES (2,1);
-INSERT INTO `sys_role_permission` (`permission_id`,`role_id`) VALUES (3,2);
-INSERT INTO `sys_user_role` (`role_id`,`uid`) VALUES (1,1);
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES (1, '管理员', 'd3c59d25033dbf980d29554025c23a75', '8d78869f470951332959580424d4bf4f', 1, 'admin');
 
 SET FOREIGN_KEY_CHECKS = 1;
