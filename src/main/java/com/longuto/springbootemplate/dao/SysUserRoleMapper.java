1f8d27c84827026e6a8f9420a2b279a583bc74f6
longuto <1592404240@qq.com>
2019-03-25 23:27:41 +0800

    bug修复

--- src/main/java/com/longuto/springbootemplate/dao/SysUserRoleMapper.java
+++ src/main/java/com/longuto/springbootemplate/dao/SysUserRoleMapper.java
@@ -3,10 +3,11 @@ package com.longuto.springbootemplate.dao;
 import com.longuto.springbootemplate.common.base.MyMapper;
 import com.longuto.springbootemplate.domain.SysUserRole;
 import com.longuto.springbootemplate.dto.LoginUserDto;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface SysUserRoleMapper extends MyMapper<SysUserRole> {
 
-    List<LoginUserDto> findUserByRole(Integer roleId);
+    List<LoginUserDto> findUserByRole(@Param("roleId") Integer roleId);
 }
