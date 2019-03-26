7351a7cf406744211e5ff05011f67d2809836cd3
longuto <1592404240@qq.com>
2019-03-26 16:22:02 +0800

    错误码提示

--- src/main/java/com/longuto/springbootemplate/common/shiro/filter/SystemLogoutFilter.java
+++ src/main/java/com/longuto/springbootemplate/common/shiro/filter/SystemLogoutFilter.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.PrintWriter;
 
+import static com.longuto.springbootemplate.common.domain.APIResponse.CODE_FAIL;
+import static com.longuto.springbootemplate.common.domain.APIResponse.CODE_SUCCESS;
+
 /**
  * 自定义退出的拦截器，退出返回json
  * @author 35168
@@ -23,13 +26,13 @@ public class SystemLogoutFilter extends LogoutFilter {
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
         Subject subject = getSubject(request, response);
-        int code = 0;
+        int code = CODE_SUCCESS;
         String msg = "成功";
         try {
             subject.logout();
         } catch (SessionException ise) {
             log.debug("Encountered session exception during logout.  This can generally safely be ignored.", ise);
-            code = 500;
+            code = CODE_FAIL;
             msg = ise.getMessage();
         }
         response.setCharacterEncoding("UTF-8");
