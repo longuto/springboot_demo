53c903dfc9c57dd7735876186fbf325ea1908d26
longuto <1592404240@qq.com>
2019-03-28 09:20:12 +0800

    excel导出工具类

---
+++ src/main/java/com/longuto/springbootemplate/common/config/SpringbootemplateConstant.java
@@ -0,0 +1,12 @@
+package com.longuto.springbootemplate.common.config;
+
+public class SpringbootemplateConstant {
+
+    private SpringbootemplateConstant() {
+
+    }
+
+    public static final String XLSX_SUFFIX = ".xlsx";
+
+    public static final String XLSX_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+}
---
+++ src/main/java/com/longuto/springbootemplate/common/utils/poi/convert/ExportConvert.java
@@ -0,0 +1,6 @@
+package com.longuto.springbootemplate.common.utils.poi.convert;
+
+public interface ExportConvert {
+	
+	String handler(Object val);
+}
